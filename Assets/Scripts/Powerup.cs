8894e1913faac1fd8be89b40c685a9380e0be683
PZX1214 <106856864+PZX1214@users.noreply.github.com>
2022-06-10 22:18:21 -0400

    Update Powerup.cs

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using System;
 using System.Threading.Tasks;
 
-
+//codes the powerup that makes the player go slow
 public class Powerup : MonoBehaviour
 {
-	private GameObject player;
-	public CameraMovement camera;
+	private GameObject player; //UFO
+	public CameraMovement camera; //creates Camera object
 
 	// Start is called before the first frame update
 	void Start()
@@ -20,13 +20,13 @@ public class Powerup : MonoBehaviour
 	{
 		if (collision.tag == "Border")
 		{
-			Destroy(this.gameObject);
+			Destroy(this.gameObject); //when powerup comes in contact with border, powerup is destroyed
 		}
 
 		else if (collision.tag == "Player")
 		{
 			camera.cameraSpeed = 5f;
-			await putDelay();
+			await putDelay(); //when powerup comes in contact with player, camera speed dec to 5f
 			resetSpeed();
 
 		}
@@ -34,11 +34,11 @@ public class Powerup : MonoBehaviour
 
 	async Task putDelay()
     {
-		await Task.Delay(10000);
+		await Task.Delay(10000); //wait time for 10 seconds
 	}
 
 	public void resetSpeed()
     {
-		camera.cameraSpeed = 15f;
+		camera.cameraSpeed = 15f; //resets speed of camera
     }
 }
