70584967784ba9fbabe4199eaf976c0bd8fde73d
PZX1214 <106856864+PZX1214@users.noreply.github.com>
2022-06-10 22:16:03 -0400

    Update MenuEvents.cs

--- Assets/Scripts/MenuEvents.cs
+++ Assets/Scripts/MenuEvents.cs
@@ -1,6 +1,6 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
-
+//allows access to the other scene home page -> game scene
 public class MenuEvents : MonoBehaviour
 {
     public void LoadLevel(int index)
